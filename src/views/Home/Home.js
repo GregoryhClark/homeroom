5efc5c5898c6b0484aa734f18d611a188fe5a9d1
Warren Reader <WarrenDReader@gmail.com>
2018-04-13 16:20:54 -0600

    Optimizations

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Chart from '../../components/Chart/Chart';
 import {connect} from 'react-redux';
 import {getUser} from '../../redux/user';
