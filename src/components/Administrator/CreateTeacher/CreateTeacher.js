79cd5e4fdb17040e280f0d9664f78d1b42c2fe82
Warren Reader <WarrenDReader@gmail.com>
2018-04-19 16:50:23 -0600

    Added functionality and started bulding out endpoint for Teacher Creation form.

--- src/components/Administrator/CreateTeacher/CreateTeacher.js
+++ src/components/Administrator/CreateTeacher/CreateTeacher.js
@@ -1,13 +1,57 @@
 //MODULES
 import React from 'react';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 //CSS, ASSETS
 import './CreateTeacher.css';
 
 //COMPONENT
 class CreateTeacher extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      newTeacher: {
+          user_photo: ''
+        , first_name: ''
+        , last_name: ''
+        , username: ''
+        , password: ''
+        , email: ''
+      }
+    }
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+  }
+
+  handleFieldChange(e, field) {
+    const newTeacher = {...this.state.newTeacher};
+    newTeacher[field] = e.target.value;
+    this.setState({newTeacher});
+  }
+
+  handleClear() {
+    const newTeacher = {
+        user_photo: 'undefined'
+      , first_name: ''
+      , last_name: ''
+      , username: ''
+      , password: ''
+      , email: ''
+    };
+    this.setState({newTeacher});
+  }
+
+  handleCreate() {
+    const newTeacher = this.state.newTeacher;
+    axios.post('/createTeacher', newTeacher).then()
+  }
+
   render() {
+
+    let {first_name, last_name, username, password, email} = this.state.newTeacher;
+
     return (
       <div className="creation-form">
         <h1>Create Teacher</h1>
@@ -20,27 +64,37 @@ class CreateTeacher extends React.Component {
 
           <div className="field">
             <span>First Name:</span>
-            <input type="text"/>
+            <input type="text" value={first_name} 
+            onChange={(e) => this.handleFieldChange(e, 'first_name')}/>
           </div>
 
           <div className="field">
             <span>Last Name:</span>
-            <input type="text"/>
+            <input type="text" value={last_name} 
+            onChange={(e) => this.handleFieldChange(e, 'last_name')}/>
           </div>
 
           <div className="field">
             <span>Username:</span>
-            <input type="text"/>
+            <input type="text" value={username} 
+            onChange={(e) => this.handleFieldChange(e, 'username')}/>
+          </div>
+
+          <div className="field">
+            <span>Password:</span>
+            <input type="text" value={password} 
+            onChange={(e) => this.handleFieldChange(e, 'password')}/>
           </div>
 
           <div className="field">
             <span>Email:</span>
-            <input type="text"/>
+            <input type="text" value={email} 
+            onChange={(e) => this.handleFieldChange(e, 'email')}/>
           </div>
 
           <div className="buttons">
-            <button className="clear">Clear</button>
-            <button className="create">Create</button>
+            <button className="clear" onClick={this.handleClear}>Clear</button>
+            <button className="create" onClick={this.handleCreate}>Create</button>
           </div>
 
       </div>
