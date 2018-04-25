a23c63ebe0c7b2b95f76ede7907f7fcf1a17a6c1
shanmike <shannonmichael77@gmail.com>
2018-04-24 18:15:22 -0600

    Student / Teacher/ Views

--- src/components/Teacher/Students/TeachStudents.js
+++ src/components/Teacher/Students/TeachStudents.js
@@ -5,9 +5,21 @@ import {getUser, getTeacher} from '../../../redux/user';
 
 class TeachStudents extends Component {
     render(){
-        console.log(this.props.teacher)
+        let teacher     = this.props.teacher
+        let courses     = teacher.getCourses.map((e,i)=>{return <button key={i}>{e.course_name}</button>})
+        let allStudents = teacher.getStudent.map((e,i)=>{return <div key={i}>{`STUDENT: ${e.first_name} ${e.last_name} in class ${e.course_name} `}</div>})
+
         return (
-            <div>Students</div>
+            <div className="course-container">
+                <div className="left-column">
+                    <h1 className="course-title">Courses</h1>
+                    {courses}
+                    <button>All Students</button>
+                    </div>
+                <div className="right-column">
+                    {allStudents}
+                </div>
+            </div>
         )
     }
 }
