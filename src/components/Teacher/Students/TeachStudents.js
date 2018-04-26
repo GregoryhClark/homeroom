c83beaf97ed8ef092d1f75723d706a2e27384a1f
shanmike <shannonmichael77@gmail.com>
2018-04-26 11:48:00 -0600

    Teachers Students Style

--- src/components/Teacher/Students/TeachStudents.js
+++ src/components/Teacher/Students/TeachStudents.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {getUser, getTeacher} from '../../../redux/user';
+import './TeachStudents.css'
 
 
 class TeachStudents extends Component {
@@ -13,13 +14,13 @@ class TeachStudents extends Component {
     }
     render(){
         let teacher       = this.props.teacher;
-        let courses       = teacher.getCourses.map((e,i)=>{return <button onClick={()=>this.setState({selectedCourse:e.course_id,courseName:e.course_name})} key={i}>{e.course_name}</button>});
+        let courses       = teacher.getCourses.map((e,i)=>{return <button className="stud_button" onClick={()=>this.setState({selectedCourse:e.course_id,courseName:e.course_name})} key={i}>{e.course_name}</button>});
         let currentCourse = teacher.getStudent.filter((e,i)=>{return e.course_id === this.state.selectedCourse}).map((e,i)=>{
             return (<tr key={i}>
                         <td>{e.first_name}</td>
                         <td>{e.last_name}</td>
                         <td>{e.course_name}</td>
-                        <td>{e.email}</td>
+                        <td><a href={`mailto:${e.email}`}>{e.email}</a></td>
                         <td>{e.user_photo === 'undefined' ? 'None' : <a href={e.user_photo} target='_blank'>View</a>}</td>     
                     </tr>)
         });
@@ -28,7 +29,7 @@ class TeachStudents extends Component {
                         <td>{e.first_name}</td>
                         <td>{e.last_name}</td>
                         <td>{e.course_name}</td>
-                        <td>{e.email}</td>
+                        <td><a href={`mailto:${e.email}`}>{e.email}</a></td>
                         <td>{e.user_photo === 'undefined' ? 'None' : <a href={e.user_photo} target='_blank'>View</a>}</td>     
                     </tr>)
         });
@@ -37,7 +38,7 @@ class TeachStudents extends Component {
                 <div className="left-column">
                     <h1 className="course-title">Courses</h1>
                     {courses}
-                    <button onClick={()=>this.setState({selectedCourse:'',courseName:''})}>All Students</button>
+                    <button className="stud_button" onClick={()=>this.setState({selectedCourse:'',courseName:''})}>All Students</button>
                     </div>
                 <div className="right-column">
                 <h1 className="course-title">{this.state.courseName === '' ? "All Students": `Students for ${this.state.courseName}`}</h1>
@@ -56,6 +57,7 @@ class TeachStudents extends Component {
                     {this.state.selectedCourse === '' ? allStudents : currentCourse}
                   </tbody>
                 </table>
+               
               </div>
                 </div>
             </div>
