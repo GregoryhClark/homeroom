46dad4251a501fdfa49f8b93db2cb48e283d916f
shanmike <shannonmichael77@gmail.com>
2018-04-26 19:52:37 -0600

    Teacher

--- src/components/Navbar/CouresModal/CoursesModal.js
+++ src/components/Navbar/CouresModal/CoursesModal.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
-import {getUser, getStudent, selectedCourse} from '../../../redux/user';
+import {getUser, getStudent, getTeacher, selectedCourse} from '../../../redux/user';
 //CSS, ASSETS
 import './CoursesModal.css';
 
@@ -10,11 +10,14 @@ import './CoursesModal.css';
 class CoursesModal extends React.Component {
   render() {
     let studentData = this.props.student;
+    let teacherData = this.props.teacher;
     let courseCards = studentData.getCourses ? studentData.getCourses.map((element, index) => {
           return <Link key={index} className="class-link" onClick={()=>this.props.selectedCourse(element.course_id)} to="courses">
                       {element.course_name}
                  </Link>
       }) : null;
+
+    console.log(teacherData)
     return (
       <div id="courses-modal" className="modal">
             <div className="modal-content" onClick={this.props.updateSecondNav}> <h1>Select a Course</h1>{courseCards}</div>
@@ -28,7 +31,8 @@ function mapStateToProps(state) {
   return {
       student: state.student
     , currentCourseID:state.currentCourseID
+    , teacher: state.teacher
   }
 }
 
-export default connect(mapStateToProps,{getUser, getStudent, selectedCourse})(CoursesModal)
+export default connect(mapStateToProps,{getUser, getStudent, getTeacher, selectedCourse})(CoursesModal)
