73797011d8972110554e4d9bb6237f417385b695
Warren Reader <WarrenDReader@gmail.com>
2018-04-27 08:29:32 -0600

    Added styling to Students my account.

--- src/components/Student/MyAccount/StudentAccount.js
+++ src/components/Student/MyAccount/StudentAccount.js
@@ -38,12 +38,12 @@ class StudentAccount extends Component{
         let {saveStatus} = this.state;
         let currentStudent = this.props.user;
         return(
-            <div className="student-account">
+            <div id="admin-account" className="student-account">
                 <Container type="container">
                     <Titles type="titles">{`${currentStudent.first_name}'s Settings`}</Titles>
                     <InnerContainer type="innerContainer">
                         {this.props.user.user_photo === 'undefined' ? <div style={noPhoto}><span style={addPhoto}>Add Photo</span></div> : <img src={this.props.user.user_photo} style={profilePicture} alt="User Profile"/>}
-                        {saveStatus === 'pending' ? <div className="save-status-pending">Pending...</div> : saveStatus === true ? <div className="save-status-successful">Save Successful</div> : null}
+                        {saveStatus === 'pending' ? <div className="save-status pending">Pending...</div> : saveStatus === true ? <div className="save-status successful">Save Successful</div> : null}
                         <InnerTitle type="innerTitle">Display Name:</InnerTitle>
                             <InputText type="inputText" stateValue={first_name} field={'first_name'} update={this.handleUpdateState}/>
                         <InnerTitle type="innerTitle">Email:</InnerTitle>
@@ -59,11 +59,12 @@ class StudentAccount extends Component{
                         </select>
                         <ButtonContainer type="buttonContainer">
                         
-                            <Button type="cancel">Cancel</Button>
                             <Button type="save" save={this.handleAccountSave}>Save</Button>
                         </ButtonContainer>
                     </InnerContainer>
                 </Container>
+
+        {/*
                 <Container type="container">
                     <Titles type="titles">Notification Preferences</Titles>
                     <InnerContainer type="innerContainer">
@@ -72,6 +73,8 @@ class StudentAccount extends Component{
                         <InnerTitle type="innerTitle">Scheduling:</InnerTitle>
                     </InnerContainer>
                 </Container>
+
+        */}
             </div>
         )
     }
@@ -124,97 +127,101 @@ function Button(props){
 // ************** STYLES ***************
 // ========= CONTAINER STYLE ===========
 const containerStyle={
-      boxSizing: "border-box"
-    , margin:"0 20px"
-    , flex:"1"
-}
-// =========== TITLE STYLE =============
-const titleStyle={
-      fontSize:"24px"
-    , paddingBottom:"3px"
-    , borderBottom:"1px solid #7D7D7D"
-}
-// =======INNER CONTAINER STYLE ========
-const innerContainerStyle={
-      backgroundColor:"#FFF"
-    , marginTop:"10px"
-    , padding:"10px"
-    , marginBottom:"15px"
-    , boxSizing: "border-box"
-}
-// ======= INNER TITLE STYLE ===========
-const innerTitleStyle={
-      fontSize:"1em"
-    , padding:"8px"
-}
-// ============ BUTTON STYLES ==========
-const buttonContainer={
-      width:"180px"
-    , display:"block"
-    , margin:" 15px 10px 5px auto"
-}
-const buttonStyle={
-      width:"80px"
-    , margin:" 0px 5px 0px"
-    , padding:"5px"
-    , fontSize:"0.85em"
-    , border:"none"
-    , borderRadius:"3px"
-    , outline:"none"
-}
-const buttonSave={
-      color:"#FFF"
-    , backgroundColor:"rgb(0, 166, 237)"
-}
-const buttonCancel={
-      color:"#000"
-    , backgroundColor:"rgb(200, 200, 200)"
-}
-// ========= INPUT TEXT STYLE ==========
-const inputTextStyle={
-      fontSize:"1em"
-    , padding:"5px"
-    , marginRight:'10px'
-    , marginLeft:'10px'
-    , border:"1px solid #eee"
-    , borderRadius:"3px"
-    , width:"calc(100% - (20px))"
-    , boxSizing:"border-box"
-}
-// ======== PROFILE PICTURE ============
-const profilePicture ={
-      width:"100%"
-    , maxWidth:"250px"
-    , borderRadius:"50%"
-    , display:"block"
-    , margin: "auto"
-    , backgroundColor:"#EEE"
-    , boxShadow:"1px 1px 15px #CCC"
-}
-const noPhoto ={
-      height:"250px"
-    , width:"250px"
-    , position:"relative"
-    , borderRadius:"50%"
-    , display:"block"
-    , margin: "auto"
-    , backgroundColor:"#EEE"
-}
-const addPhoto ={
-      color:"#7d7d7d"
-    , position:"absolute"
-    , top:"104px"
-    , fontSize:"2em"
-    , left:"52px"
-}
-// ************ STYLE LOOKUP ***********
-const styleLookup={
-      container:containerStyle
-    , titles:titleStyle
-    , innerContainer:innerContainerStyle
-    , innerTitle:innerTitleStyle
-    , inputText: inputTextStyle
-    , buttonContainer: buttonContainer
-    , save: buttonSave
-    , cancel: buttonCancel
-}
+  padding: '20px'
+      , boxSizing: "border-box"
+      , margin:"0 20px"
+      , flex:"1"
+  }
+  // =========== TITLE STYLE =============
+  const titleStyle={
+        fontSize:"24px"
+      , color:"#7d7d7d"
+      , paddingBottom:"3px"
+      , borderBottom:"1px solid #7D7D7D"
+      , textAlign:"center"
+  }
+  // =======INNER CONTAINER STYLE ========
+  const innerContainerStyle={
+        backgroundColor:"rgb(242, 242, 242)"
+      , marginTop:"10px"
+      , padding:"10px"
+      , marginBottom:"5px"
+      , boxSizing: "border-box"
+  }
+  // ======= INNER TITLE STYLE ===========
+  const innerTitleStyle={
+        fontSize:"1em"
+      , color:"#7d7d7d"
+      , padding:"8px"
+  }
+  // ============ BUTTON STYLES ==========
+  const buttonContainer={
+        width:"80px"
+      , display:"block"
+      , margin:" 35px 10px 5px auto"
+  }
+  const buttonStyle={
+        width:"80px"
+      , margin:" 0px 5px 0px"
+      , padding:"8px"
+      , fontSize:"0.9em"
+      , border:"none"
+      , borderRadius:"3px"
+      , outline:"none"
+  }
+  const buttonSave={
+        color:"#FFF"
+      , backgroundColor:"rgb(0, 166, 237)"
+  }
+  const buttonCancel={
+        color:"#000"
+      , backgroundColor:"rgb(200, 200, 200)"
+  }
+  // ========= INPUT TEXT STYLE ==========
+  const inputTextStyle={
+        fontSize:"1em"
+      , padding:"5px"
+      , marginRight:'10px'
+      , marginLeft:'10px'
+      , border: "1px solid lightgray"
+      , borderRadius:"3px"
+      , width:"calc(100% - (20px))"
+      , boxSizing:"border-box"
+  }
+  // ======== PROFILE PICTURE ============
+  const profilePicture ={
+        width:"100%"
+      , maxWidth:"250px"
+      , borderRadius:"50%"
+      , display:"block"
+      , margin: "auto"
+      , backgroundColor:"#EEE"
+      , boxShadow:"1px 1px 15px #CCC"
+  }
+  const noPhoto ={
+        height:"250px"
+      , width:"250px"
+      , position:"relative"
+      , borderRadius:"50%"
+      , display:"block"
+      , margin: "auto"
+      , backgroundColor:"#EEE"
+  }
+  const addPhoto ={
+        color:"#7d7d7d"
+      , position:"absolute"
+      , top:"104px"
+      , fontSize:"2em"
+      , left:"52px"
+  }
+  // ************ STYLE LOOKUP ***********
+  const styleLookup={
+        container:containerStyle
+      , titles:titleStyle
+      , innerContainer:innerContainerStyle
+      , innerTitle:innerTitleStyle
+      , inputText: inputTextStyle
+      , buttonContainer: buttonContainer
+      , save: buttonSave
+      , cancel: buttonCancel
+  }
